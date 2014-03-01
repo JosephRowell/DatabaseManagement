2caed16ceba9c5472e28e1bee8a1885b8b5f5428
JosephRowell <rowelljoey@yahoo.com>
2014-03-01 12:52:53 -0500

    Lab5
    Lab5 fixed for error and to look pretty

--- dbm5/lab5.sql
+++ dbm5/lab5.sql
@@ -1,38 +1,40 @@
 --1--
 
 select a.city from agents a
-inner join orders o on o.aid = a.aid
-inner join customers c on o.cid = c.cid
-where c.name = 'Basics'
+	inner join orders o on o.aid = a.aid
+	inner join customers c on o.cid = c.cid
+		where c.name = 'Basics';
 
 --2--
 
 select distinct pid from orders o
-inner join customers c on o.cid = c.cid
-where city = 'Kyoto'
+	inner join customers c on o.cid = c.cid
+		where city = 'Kyoto'
 
 
 --3--
 
 select name from customers
-where cid not in (select cid from orders)
+	where cid not in (select cid from orders);
 
 --4--
 
 select name from customers
-left outer join orders
-on orders.cid = customers.cid
-where orders.cid is null
+	left outer join orders on orders.cid = customers.cid
+		where orders.cid is null;
 
 --5--
 
 --Get	the	names	of	customers	who	placed	at	least	one	order	through	an	agent	in	their	
 --city,	along	with	those	agent(s’)	names.
---(I will need to correct this because it is wrong) --
+--corrected --
+
+
 select distinct c.name, a.name
-from customers c, agents a
-inner join orders o on o.aid = a.aid
-where c.city = a.city
+	from agents a
+		inner join orders o on o.aid = a.aid
+		inner join customers c on c.cid = o.cid
+			where c.city = a.city;
 	
 
 
@@ -41,12 +43,12 @@ where c.city = a.city
 select c1.name, c1.city, a1.name, a1.city
 	from customers c1,
 		agents a1
-where c1.city = a1.city
+			where c1.city = a1.city;
 
 --7--
 
 select city, sum(quantity) as "sq"
-from products
-group by city
-order by sq asc
-limit 1
+	from products
+		group by city
+		order by sq asc
+	limit 1
